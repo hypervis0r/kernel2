6e4728120a14ba64bd75dfef1f2cf2c26f5d8c07
hypervis0r <hyp3rvisor@protonmail.com>
2021-07-18 16:17:16 -0700

    implement ExitBootServices

--- src/boot/boot.c
+++ src/boot/boot.c
@@ -13,6 +13,11 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
     EFI_SERIAL_IO_PROTOCOL *lpSerialProtocol = NULL;
     KE_RECT rect = { 0 };
     KE_GOP_INFO gopInfo = { 0 };
+    UINTN MemoryMapSize = 0;
+    EFI_MEMORY_DESCRIPTOR *MemoryMap = NULL;
+    UINTN LocalMapKey = 0;
+    UINTN DescriptorSize = 0;
+    UINT32 DescriptorVersion = 0;
 
     KeBootGetSerialProtocol(SystemTable, &lpSerialProtocol);
     KeBootSerialWrite(lpSerialProtocol, L"[+] Serial Hello World\n", 48);
@@ -31,12 +36,44 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 
     KeBootClearScreen(&gopInfo, 0);
 
-    KeBootPrintDebug(SystemTable, L"Fuck you");
+    KeBootPrintDebug(SystemTable, L"Fuck you\n");
 
     /*
         Wait for keystroke
     */
     while ((Status = SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key)) == EFI_NOT_READY) ;
- 
+
+    /*
+        Get memory map.
+        Ironically, calling GetMemoryMap will require you to allocate memory for the map itself, 
+        which, in turn, will change the memory map.
+    */
+    do {  
+        Status = SystemTable->BootServices->GetMemoryMap(
+                &MemoryMapSize, 
+                MemoryMap, 
+                &LocalMapKey, 
+                &DescriptorSize,
+                &DescriptorVersion);
+        if (Status == EFI_BUFFER_TOO_SMALL){
+            SystemTable->BootServices->AllocatePool(EfiLoaderData, MemoryMapSize + 1, &MemoryMap);
+            Status = SystemTable->BootServices->GetMemoryMap(
+                    &MemoryMapSize, 
+                    MemoryMap, 
+                    &LocalMapKey, 
+                    &DescriptorSize,
+                    &DescriptorVersion);      
+        }
+        //DbgPrint(L"This time through the memory map loop, status = %r\n",Status);
+    } while (Status != EFI_SUCCESS);
+
+    /*
+        Exit the bootloader and jump to the kernel
+    */
+    Status = SystemTable->BootServices->ExitBootServices(ImageHandle, MemoryMap);
+
+    // TODO: Jump to kernel kek
+    KeBootSerialWrite(lpSerialProtocol, L"We left the loader, now in kernel (not really)\n", 50);
+
     return Status;
 }
